0f80ca68416329024e266c17b2f50632bed50550
Michal Schorm <mschorm@redhat.com>
2019-11-04 02:31:41 +0100

    Update

--- setup.sh
+++ setup.sh
@@ -14,23 +14,23 @@ set -xv
 # CLI software
 SHELL="bash bash-completion"
 GIT="git gitg"
-COMPILE_TOOLSET="gcc-c++ make cmake gdb"
+COMPILE_TOOLSET="gcc gcc-c++ make cmake gdb"
 EDITOR="vim nano"
 SSH="openssh openssh-server"
-TOOLS="mc tree ntfs-3g zip unzip tar"
+TOOLS="mc tree ntfs-3g zip unzip tar wget curl lshw"
 PYTHON="python python2 python3"
 
-dnf install -y --nogpgcheck $SHELL $GIT $COMPILE_TOOLSET $EDITOR $SSH $TOOLS $PYTHON
+dnf install -y --nogpgcheck $SHELL $GIT $COMPILE_TOOLSET $EDITOR $SSH $TOOLS $PYTHON || exit
 
 # More repositories
 RPMFUSION="https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 REPOS="fedora-workstation-repositories fedora-repos-rawhide $RPMFUSION"
 
-dnf install -y --nogpgcheck $REPOS
+dnf install -y --nogpgcheck $REPOS || exit
 
 # GUI software
 GUI_EDITOR="gedit"
-WEB_BROWSER="google-chrome-stable firefox"
+WEB_BROWSER="chromium firefox"
 IRC="hexchat"
 OFFICE="libreoffice"
 TERMINAL="cool-retro-term"
@@ -38,17 +38,25 @@ VIDEO="vlc"
 # Desktop environments
 DE="@cinnamon-desktop-environment @lxde-desktop-environment"
 
-dnf install -y --nogpgcheck $GUI_EDITOR $WEB_BROWSER $IRC $OFFICE $TERMINAL $DE $VIDEO
+dnf install -y --nogpgcheck $GUI_EDITOR $WEB_BROWSER $IRC $OFFICE $TERMINAL $VIDEO $DE || exit
 
 
-
-dnf remove -y "xed dnfdragora"
+GARBAGE="dnfdragora shotwell pidgin thunderbird leafpad galculator xpad xfburn parole xawtv xed"
+for pkg in $GARBAGE ; do dnf remove -y $pkg; done
 
 dnf update -y --nogpgcheck
 
 # --------------------------------
 
-# start sshd
-# print IP
-# uzivatele, jmena, hesla
-# klavesnice
+# Start SSH server
+systemctl enable sshd
+# some sed ?
+systemctl start sshd
+
+# --------------------------------
+
+# Add user "lod"
+useradd -m lod && echo -e "lod:lod" | chpasswd lod
+usermod -c "Hvězdná Loď" lod
+
+# --------------------------------
